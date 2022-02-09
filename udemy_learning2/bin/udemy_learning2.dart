867cf994e670f2e20aac0859e30dcb9400c3cb19
shiawyonglim <65016718+shiawyonglim@users.noreply.github.com>
2022-02-09 20:04:10 +0800

    Add files via upload

---
+++ udemy_learning2/bin/udemy_learning2.dart
@@ -0,0 +1,5 @@
+import 'package:udemy_learning2/udemy_learning2.dart' as udemy_learning2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${udemy_learning2.calculate()}!');
+}
---
+++ udemy_learning2/test/udemy_learning2_test.dart
@@ -0,0 +1,8 @@
+import 'package:udemy_learning2/udemy_learning2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
